c5f7eae66b2ca9ead079eae0e087bff53a498950
Bharath Nair <bharath.rajeevnair@student.adelaide.edu.au>
2021-03-06 00:41:08 +1030

    fastai errors

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 from typing import Optional
-
-from fastapi import FastAPI
-
+import wave
+import os
+import pylab
+import uuid
+from fastapi import FastAPI, File, UploadFile
+from fastai import *
+from fastai.vision import *
+from fastai import load_model
+from io import BytesIO
 app = FastAPI()
+
+learn = load_model("model/stage-1.pth")
+
+def get_wav_info(wav_file):
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pylab.fromstring(frames, 'int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    return sound_info, frame_rate
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"status": "online"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/analyze")
+async def analyze_audio(file: UploadFile = File(...)):
+    print(file.filename)
+    wav_f = file.file
+    sound_info, frame_rate = get_wav_info(wav_f)
+    pylab.figure(num=None, figsize=(19, 12))
+    pylab.subplot(111)
+    pylab.specgram(sound_info, Fs=frame_rate)
+    filename = str(uuid.uuid4())
+    pylab.savefig('tmp/'+filename+".png")
+    return {"item_id": "", "q": ""}
